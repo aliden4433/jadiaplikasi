34dfcdd6355df6fb8ba378084d81c3520e03214f
Zflip5 <zflip2084@gmail.com>
2025-07-06 04:46:35 +0000

    hubungkan dengan database api key ini: apiKey: "AIzaSyDtoNJt4eIyOBVbNuSq

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-}
+  apiKey: "AIzaSyDtoNJt4eIyOBVbNuSqHzC-XUpl-bqhkYc",
+  authDomain: "projeck1-3333d.firebaseapp.com",
+  projectId: "projeck1-3333d",
+  storageBucket: "projeck1-3333d.appspot.com",
+  messagingSenderId: "407147695851",
+  appId: "1:407147695851:web:8f04030ddf719febb478ae"
+};
 
 // Initialize Firebase for SSR
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
