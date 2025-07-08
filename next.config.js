cf72f0bea2c2dbe428b546aa53acf423e3a9060c
Zflip5 <zflip2084@gmail.com>
2025-07-08 15:20:15 +0000

    [22:17:31.938] Running build in Washington, D.C., USA (East) – iad1 [22:

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+const runtimeCaching = require("next-pwa/cache");
+
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-  extendDefaultRuntimeCaching: true,
+  runtimeCaching,
 });
 
 const nextConfig = {
